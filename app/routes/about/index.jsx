964e7fa4dbf6597cc16cc21aa226afb419e5b19b
Siyam <itsyakubsiyam@gmail.com>
2021-12-10 15:43:36 +0600

    update about page

--- app/routes/about/index.jsx
+++ app/routes/about/index.jsx
@@ -5,6 +5,10 @@ export default function About() {
     <div className="row justify-content-between align-items-center">
       <div className="col-md-6">
         <h1>Why Use Polyblog?</h1>
+        <p>
+          Grow your business with content marketing. Reach an international
+          audience and grow your business drastically.
+        </p>
       </div>
       <div className="col-md-6">
         <img src={aboutUSImg} alt="About Us Img" className="img-fluid" />
