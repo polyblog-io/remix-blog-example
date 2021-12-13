363c58fa6dcf945dbf375508dd6376bb3c781d20
Siyam <itsyakubsiyam@gmail.com>
2021-12-13 15:36:39 +0600

    fix fetching blog

---
+++ app/routes/blogs/newBlog.tsx
@@ -0,0 +1,24 @@
+export default function NewBlogRoute() {
+  return (
+    <div>
+      <p>Add your own blog</p>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
